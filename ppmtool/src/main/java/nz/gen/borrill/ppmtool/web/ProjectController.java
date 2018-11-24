7144008b20671cfbc51847ae92e60e21b2f47914
Chris Borrill <chris.borrill@gmail.com>
2018-11-25 11:21:56 +1300

    Improve validation error display in JSON

--- ppmtool/src/main/java/nz/gen/borrill/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/nz/gen/borrill/ppmtool/web/ProjectController.java
@@ -1,11 +1,17 @@
 package nz.gen.borrill.ppmtool.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +30,9 @@ public class ProjectController {
 	@PostMapping("")
 	public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
 		if (result.hasErrors()) {
-			return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
+			Map<String, String> errors = 
+				result.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));			
+			return new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
 		}
 		Project newproject = this.projectService.saveOrUpdate(project);
 		return new ResponseEntity<Project>(newproject, HttpStatus.CREATED);
