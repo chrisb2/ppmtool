69ec7696dc3821d6eaccd450e006566c94fc580d
Chris Borrill <chris.borrill@gmail.com>
2018-11-25 10:51:53 +1300

    Update validation using BindingResult

--- ppmtool/src/main/java/nz/gen/borrill/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/nz/gen/borrill/ppmtool/web/ProjectController.java
@@ -1,8 +1,11 @@
 package nz.gen.borrill.ppmtool.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,10 @@ public class ProjectController {
 	private ProjectService projectService;
 
 	@PostMapping("")
-	public ResponseEntity<Project> createProject(@RequestBody Project project) {
+	public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
+		if (result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
+		}
 		Project newproject = this.projectService.saveOrUpdate(project);
 		return new ResponseEntity<Project>(newproject, HttpStatus.CREATED);
 	}
