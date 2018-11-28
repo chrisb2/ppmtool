49411d66f1173393713f9ef72961ec4ec85bb8a3
Chris Borrill <chris.borrill@gmail.com>
2018-11-28 19:09:10 +1300

    Fix 500 error with fields having multiple validation errors

--- ppmtool/src/main/java/nz/gen/borrill/ppmtool/domain/ProjectKey.java
+++ ppmtool/src/main/java/nz/gen/borrill/ppmtool/domain/ProjectKey.java
@@ -2,11 +2,13 @@ package nz.gen.borrill.ppmtool.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Embeddable
 public class ProjectKey {
 	
+	@NotBlank
 	@Size(min=4, max=5, message="Project key must be 4 to 5 characters")
 	@Column(name="projectKey", updatable=false, unique=true)	
 	private String value;
--- ppmtool/src/main/java/nz/gen/borrill/ppmtool/services/MapValidationErrorService.java
+++ ppmtool/src/main/java/nz/gen/borrill/ppmtool/services/MapValidationErrorService.java
@@ -8,15 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import static java.util.stream.Collectors.toMap;
 
 @Service
 public class MapValidationErrorService {
 	
 	public ResponseEntity<?> getErrorResponse(BindingResult result) {
 		if (result.hasErrors()) {
-			// TODO - this will fail if a single field can have multiple validation failures!
+			// TODO - conversion to map throws away errors if there is more one for a field
 			Map<String, String> errors = 
-				result.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));			
+				result.getFieldErrors().stream().collect(toMap(FieldError::getField, FieldError::getDefaultMessage, (e1, e2) -> e1));			
 			return new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
 		}
 		return null;
