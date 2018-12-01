ed7934ac2af5636f6d5856d3a5ee7d31ceb4db75
Chris Borrill <chris.borrill@gmail.com>
2018-12-02 10:22:44 +1300

    Refactor NewProjectDto

--- ppmtool/src/main/java/nz/gen/borrill/ppmtool/web/NewProjectDto.java
+++ ppmtool/src/main/java/nz/gen/borrill/ppmtool/web/NewProjectDto.java
@@ -1,40 +1,16 @@
 package nz.gen.borrill.ppmtool.web;
 
-import java.util.Date;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import nz.gen.borrill.ppmtool.domain.Project;
 
-class NewProjectDto {
+class NewProjectDto extends UpdateProjectDto {
 	
 	@NotBlank
 	@Size(min=4, max=5, message="Project key must be 4 to 5 characters")
 	private String projectKey;
-	
-	@NotBlank(message="Project name is required")
-	private String projectName;
-	
-	@NotBlank(message="Project description is required")
-	private String description;
-	
-	@JsonFormat(pattern=ProjectDto.DATE_FORMAT)
-	private Date startDate;
-	@JsonFormat(pattern=ProjectDto.DATE_FORMAT)
-	private Date endDate;
-	
+		
 	NewProjectDto() {}
-
-	public String getProjectName() {
-		return projectName;
-	}
-
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
 
 	public String getProjectKey() {
 		return projectKey;
@@ -43,33 +19,9 @@ class NewProjectDto {
 	public void setProjectKey(String projectKey) {
 		this.projectKey = projectKey;
 	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
 	
 	Project getProject() {
-		return new Project(this.projectKey, this.projectName, this.description, this.startDate, this.endDate);
+		return new Project(this.projectKey, getProjectName(), getDescription(), getStartDate(), getEndDate());
 	}
 
 }
