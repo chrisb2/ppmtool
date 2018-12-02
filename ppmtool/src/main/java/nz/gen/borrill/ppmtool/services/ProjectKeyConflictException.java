f3bb44815783d096633c847a63cd88ac9b2ade7c
Chris Borrill <chris.borrill@gmail.com>
2018-12-02 13:57:55 +1300

    Add missing inner exception

--- ppmtool/src/main/java/nz/gen/borrill/ppmtool/services/ProjectKeyConflictException.java
+++ ppmtool/src/main/java/nz/gen/borrill/ppmtool/services/ProjectKeyConflictException.java
@@ -5,7 +5,7 @@ public class ProjectKeyConflictException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public ProjectKeyConflictException(String projectKey, Exception ex) {
-		super(String.format("Project with key '%s' already exists", projectKey));
+		super(String.format("Project with key '%s' already exists", projectKey) ,ex);
 	}
 	
 
