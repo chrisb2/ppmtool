ceaec9963ca5f7070a43aa8ddc1b1aecf97fd884
Chris Borrill <chris.borrill@gmail.com>
2018-12-08 19:14:35 +1300

    Added add project and dashboard routes

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Headers from "./components/layout/Headers";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Headers />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Headers />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
