9bc42cf5abd3b9f0bef1e2b25519d6955dd4714e
Chris Borrill <chris.borrill@gmail.com>
2018-12-16 13:40:43 +1300

    Configure redux

--- ppmtool-client/src/App.js
+++ ppmtool-client/src/App.js
@@ -4,17 +4,21 @@ import Dashboard from "./components/Dashboard";
 import Headers from "./components/layout/Headers";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Headers />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Headers />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
